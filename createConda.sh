5d229838d757d57aca64058a68e11d6d5b169755
Andre <andreoorschot@gmail.com>
2020-03-30 14:21:48 -0300

    V1.2.0a0

--- createConda.sh
+++ createConda.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-conda create -n nnpoc tensorflow=2.1.0 pandas scikit-learn numpy seaborn notebook tensorflow-probability scipy keras xlrd
+conda create -n nnpoc tensorflow=2.1.0 pandas scikit-learn numpy seaborn notebook tensorflow-probability scipy keras xlrd python-graphviz jupyterlab nodejs xeus-python -c conda-forge
 exit 0
